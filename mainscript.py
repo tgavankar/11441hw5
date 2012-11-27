aa835d450385c41189d2c30dd1ebf2d1a902fe0f
tgavankar <tgavankar@gmail.com>
2012-11-27 02:00:09 -0500

    broken normalization

--- mainscript.py
+++ mainscript.py
@@ -1,18 +1,11 @@
 from __future__ import division
+from collections import defaultdict
 import heapq
 import math
 import os
 import sys
-
-
-
-from sys import getsizeof, stderr
-from itertools import chain
-from collections import deque
-try:
-    from reprlib import repr
-except ImportError:
-    pass
+import time
+import pickle
 
 
 # Caches
@@ -22,6 +15,10 @@ magCache = {}
 
 dotphit = 0
 dotpmiss = 0
+
+################
+# Data Parsing #
+################
 
 def parseData():
 	queries = []
@@ -58,7 +55,14 @@ def parseData():
 
 					users[user][movId] = rating
 
+	#movies = normalize(movies, 17770)
+	users = normalize(users, 17770)
+
 	return (queries, movies, users)
+
+##############
+# Decorators #
+##############
 
 def makeSymmetric(fn):
 	def wrapped(a, b):
@@ -67,33 +71,60 @@ def makeSymmetric(fn):
 		return fn(a, b)
 	return wrapped
 
+def timeIt(fn):
+	def wrapped(data):
+		start = time.time()
+		out = fn(data)
+		end = time.time()
+		print "Runtime: %d sec" % (end - start)
+		return out
+	return wrapped
+
+##################
+# Vector Helpers #
+##################
+
 @makeSymmetric
 def cosSim(a, b):
 	return dotp(a, b) / (mag(a) * mag(b))
 
 def getIntersection(a, b):
-	return set().union(a, b)
-	#return set.intersection(set(a.keys()), set(b.keys()))
+	return [key for key in a if key in b]
 
 def dotp(a, b):
-	#global dotphit
-	#global dotpmiss
-	#cacheKey = (tuple(a), tuple(b))
-	#if cacheKey in dotpCache:
-	#	dotphit += 1
-	#	return dotpCache[cacheKey]
-	#dotpmiss += 1
-	score = sum([a.get(key, 0) * b.get(key, 0) for key in getIntersection(a, b)])
-	#dotpCache[cacheKey] = score
+	# Don't cache because it actually slows down.
+	# Not emptying cache causes Memory Error.
+	# Emptying cache on each iteration causes only 0.02
+	# cache hit rate.
+	score = sum([a[key] * b[key] for key in getIntersection(a, b)])
 	return score
 
 def mag(a):
+	# Cache this because it is very small.
 	cacheKey = tuple(a)
 	if cacheKey in magCache:
 		return magCache[cacheKey]
-	out = math.sqrt(sum([a[key] ** 2 for key in a.keys()]))
+	out = math.sqrt(sum([a[key] ** 2 for key in a]))
 	magCache[cacheKey] = out
 	return out
+
+def normalize(data, n):
+	for vect in data:
+		xbar = (1 / n) * sum([data[vect][key] for key in data[vect]])
+		default = -1 * xbar
+		xpmag = math.sqrt(sum([(data[vect].get(key, 0) - xbar) ** 2 for key in xrange(1, n+1)]))
+		xp = [(data[vect][key] - xbar) for key in data[vect]]
+
+		for key in data[vect]:
+			data[vect][key] = (data[vect][key] - xbar) / xpmag
+
+		data[vect] = defaultdict(lambda: default, data[vect])
+
+	return data
+
+###############
+# kNN Helpers #
+###############
 
 def getNeighbors(cent, sim, k):
 	simScores = {}
@@ -106,40 +137,32 @@ def getNeighbors(cent, sim, k):
 	for (nid, rating) in dataset.items():
 		simScores[nid] = sim(dataset[centId], rating)
 	neighborKeys = heapq.nlargest(k, dict((key, val) for (key, val) in simScores.items() if otherId in dataset[key]), key=lambda nid: simScores[nid])
-	#print neighborKeys
+
 	out = [(n[0], n[1], n[2]) for n in [{idPos: nid, otherPos: otherId, 2: dataset[nid][otherId] if otherId in dataset[nid] else 0} for nid in neighborKeys]]
-	#print out
+
 	return out
 
 def getPrediction(neighbors):
 	return sum([n[2] for n in neighbors]) / len(neighbors)
 
-def kNN(movId, userId, movies, users):
+def kNN(cent):
 	k = 10
 	sim = cosSim
-	userCent = {'id': userId,
-			'other': movId,
-			'idPos': 1,
-			'otherPos': 0,
-			'dataset': users
-			}
-	movieCent = {'id': movId,
-			'other': userId,
-			'idPos': 0,
-			'otherPos': 1,
-			'dataset': movies
-			}
 
-	cent = userCent
-	#cent = movieCent
 	neighbors = getNeighbors(cent, sim, k)
 
 	return getPrediction(neighbors)
 
+########
+# main #
+########
+
+@timeIt
 def main(parsedData):
 	(queries, movies, users) = parsedData
-	global dotpCache
-	global magCache
+
+	print sum([users[1234576][k] for k in xrange(1, 17771)])
+	return
 
 	with file("output.txt", "w") as fout:
 		prevMovId = -1
@@ -147,64 +170,34 @@ def main(parsedData):
 
 		count = 0
 		for (movId, userId) in queries:
-			if count == 6:
-				prediction = kNN(movId, userId, movies, users)
-			else:
-				prediction = 0
-			print "M: %d, U: %d, R: %f" % (movId, userId, prediction)
+			userCent = {'id': userId,
+						'other': movId,
+						'idPos': 1,
+						'otherPos': 0,
+						'dataset': users
+						}
+
+			movieCent = {'id': movId,
+						'other': userId,
+						'idPos': 0,
+						'otherPos': 1,
+						'dataset': movies
+						}
+			prediction = kNN(userCent)
+
+			#print "M: %d, U: %d, R: %f" % (movId, userId, prediction)
+
 			currMovId = movId
 			if prevMovId != currMovId:
 				# New Movie
 				fout.write("%d:\n" % currMovId)
+				print "M: %d" % currMovId
 				count += 1
 			fout.write("%f\n" % prediction)
 			prevMovId = currMovId
 
-			#dotpCache = {}
-
-			if count > 6:
-				break
-
-	#print "dotp cache perf: %f, %f v %f" % (dotphit / (dotpmiss + dotphit), dotphit, dotpmiss)
-
-
-
-def total_size(o, handlers={}):
-    """ Returns the approximate memory footprint an object and all of its contents.
-
-    Automatically finds the contents of the following builtin containers and
-    their subclasses:  tuple, list, deque, dict, set and frozenset.
-    To search other containers, add handlers to iterate over their contents:
-
-        handlers = {SomeContainerClass: iter,
-                    OtherContainerClass: OtherContainerClass.get_elements}
-
-    """
-    dict_handler = lambda d: chain.from_iterable(d.items())
-    all_handlers = {tuple: iter,
-                    list: iter,
-                    deque: iter,
-                    dict: dict_handler,
-                    set: iter,
-                    frozenset: iter,
-                   }
-    all_handlers.update(handlers)     # user handlers take precedence
-    seen = set()                      # track which object id's have already been seen
-    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
-
-    def sizeof(o):
-        if id(o) in seen:       # do not double count the same object
-            return 0
-        seen.add(id(o))
-        s = getsizeof(o, default_size)
-
-        for typ, handler in all_handlers.items():
-            if isinstance(o, typ):
-                s += sum(map(sizeof, handler(o)))
-                break
-        return s
-
-    return sizeof(o)
+			#if count > 6:
+			#	break
 
 
 if __name__ == '__main__':
